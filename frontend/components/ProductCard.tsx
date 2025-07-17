1a9b91d730e506449f15fe48b29a74168488cadd
sandeep <sandeeppatil10705@gmail.com>
2025-07-17 19:14:53 +0530

    Update UI of ProductCard

--- frontend/components/ProductCard.tsx
+++ frontend/components/ProductCard.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Image from 'next/image';
-import { Heart } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Product } from '@/types';
 
@@ -11,7 +10,7 @@ type Props = {
 
 const ProductCard: React.FC<Props> = ({ product }) => {
   return (
-    <div className="rounded-xl bg-white shadow-md hover:shadow-lg transition p-4 relative flex flex-col items-center text-center space-y-4">
+    <div className="rounded-xl bg-white shadow-md hover:shadow-lg hover:shadow-black/40  transition p-4 relative flex flex-col items-center text-center space-y-4">
     
 
       {/* Product Image */}
@@ -20,24 +19,23 @@ const ProductCard: React.FC<Props> = ({ product }) => {
           src={product.image}
           alt={product.title}
           fill
-          className="object-contain bg-transparent"
+          className="object-contain "
           sizes="(max-width: 800px) 100vw, 33vw"
           priority
         />
       </div>
 
       {/* Product Title */}
-      <h3 className="text-lg font-medium text-gray-800">{product.title}</h3>
+      <h3 className="text-lg font-medium">{product.title}</h3>
 
       {/* Product Description */}
-      <p className="text-sm text-black">{product.description}</p>
+      <p className="text-sm">{product.description}</p>
 
       {/* Product Price */}
-      <p className="text-lg font-bold text-black">₹{product.price}</p>
+      <p className="text-lg font-bold">₹{product.price}</p>
 
-      
       {/* Buy Now Button */}
-      <Button className="w-50 bg-black text-white hover:bg-gray-700">
+      <Button className="w-50  text-white bg-[#0066DA] hover:bg-[#2684FC]">
         Buy Now
       </Button>
     </div>
